a46f5a8b4a39fb711419a8f6e0beb5c99fe63ce5
Brian Luft <brian@lincolnloop.com>
2015-04-02 15:51:49 -0700

    Randomize and center head dimensions for default head shape

--- index.jsx
+++ index.jsx
@@ -6,6 +6,11 @@ var Ears = require('./parts/Ears')
 var Collar = require('./parts/Collar')
 
 
+function getRandomInt(min, max) {
+  return Math.floor(Math.random() * (max - min)) + min;
+}
+
+
 var RandomBotAvatar = React.createClass({
 
   layoutParts: function() {
@@ -16,7 +21,17 @@ var RandomBotAvatar = React.createClass({
       height: 1000
     }
 
-    var head = <Head/>
+    var headWidth = getRandomInt(viewBox.width * 0.4, viewBox.width * 0.8)
+    var headX = (viewBox.width - headWidth) / 2
+    var headHeight = getRandomInt(viewBox.height * 0.4, viewBox.width * 0.8)
+    var headY = (viewBox.height - headHeight) / 2
+    var headDims = {
+      width: headWidth,
+      height: headHeight,
+      x: headX,
+      y: headY
+    }
+
     var eyes = <Eyes/>
     var top = <Top/>
     var ears = <Ears/>
@@ -25,7 +40,7 @@ var RandomBotAvatar = React.createClass({
     return {
       viewBox: viewBox,
       viewBoxParam: [String(viewBox.minX), String(viewBox.minY), String(viewBox.width), String(viewBox.height)].join(' '),
-      head: head,
+      headDims: headDims,
       eyes: eyes,
       top: top,
       ears: ears,
@@ -39,7 +54,11 @@ var RandomBotAvatar = React.createClass({
       <svg
         viewBox={layout.viewBoxParam}
         {...this.props}>
-        {layout.head}
+
+        {/* placeholder background */}
+        <rect x="0" y="0" width="1000" height="1000" fill="#CCCCCC"/>
+
+        <Head {...layout.headDims}/>
         {layout.eyes}
         {layout.top}
         {layout.ears}
